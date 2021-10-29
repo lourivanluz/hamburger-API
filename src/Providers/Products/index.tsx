52f3cf41105fd7c366e55dfc5775c1704546f4a7
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-10-29 16:21:19 -0300

    feat cart provider implementado

---
+++ src/Providers/Products/index.tsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface product {
+  id: string;
+  name: string;
+  price: number;
+  type: string;
+  img: string;
+  quantity?: number;
+  userId?: number;
+}
+interface ProductProviderProps {
+  children: ReactNode;
+}
+
+interface ProductProviderData {
+  products: product[];
+  addProduct: (data: product) => void;
+  deleteProduct: (idProduct: string) => void;
+}
+
+export const ProductContext = createContext<ProductProviderData>(
+  {} as ProductProviderData
+);
+
+export const ProductProvider = ({ children }: ProductProviderProps) => {
+  const [products, setProducts] = useState<product[]>([] as product[]);
+  const [token] = useState(localStorage.getItem("@tokenKH") || "");
+
+  useEffect(() => {
+    axios
+      .get("/products")
+      .then((response) => setProducts(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const addProduct = (data: product) => {
+    data.userId = 1;
+    axios
+      .post("/products", data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => console.log("adicionado produto novo"))
+      .catch((err) => console.log("erro a cadastrar novo produto"));
+  };
+
+  const deleteProduct = (idProduct: string) => {
+    axios
+      .delete(`/products/${idProduct}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => console.log("deletado"))
+      .catch((err) => console.log("nao deletado"));
+  };
+
+  return (
+    <ProductContext.Provider value={{ products, addProduct, deleteProduct }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export const useProduct = () => useContext(ProductContext);
