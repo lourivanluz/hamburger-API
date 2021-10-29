166a5adf5c9d4b99298a09ff7a280197f474e544
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-10-29 13:07:04 -0300

    feat router implemented

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,3 +1,24 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Pages/Home";
+import { Login } from "../Pages/Login";
+import { Products } from "../Pages/Products";
+import { Register } from "../Pages/Register";
+
 export const Routers = () => {
-  return <div>a</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </Switch>
+  );
 };
